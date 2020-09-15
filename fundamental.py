d9d0386b1b095f5097b86dc0f03d1783110bd07d
Patrick Rodrigues <47316946+RodPaDev@users.noreply.github.com>
2020-09-15 19:18:46 +0100

    add fundamental exercices

---
+++ fundamental.py
@@ -0,0 +1,68 @@
+## 1. Without looking at previous examples what are some of the common data types ?
+
+## 2. What is the difference between = and == ?
+
+## 3. What is a variable? What can you do with one?
+
+## 4. What are all of the logic operators? (google it if you need)
+
+## 5. What are all of the arithmetic operators? (google it if you need)
+
+## 6. How do you define a function?
+
+## 7. What is a parameter?
+
+## 8. How do you call/invoke a function?
+
+## 9. What are logic statements?
+
+## 10. Does the order of logic statements matter? 
+
+## 11. What matters more the readability of your code or how good it is? 
+
+## 12. Pick one of your favourite applications/games and try to explain to me the best you can how it works
+
+###############
+## EXERCICES ##
+###############
+
+## 1. Write functions for the following operations add, subtract, multiply, divide 
+## The function should take two parameters: number1, number2 and return the value of the operation
+
+## 2. Finish the following function that does all of the arithmethic operations above by using logic statement, it should return the value of the operation
+
+def evaluate(arithmeticOperation, num1, num2):
+    pass
+
+## 3. Write a function that determines whether a number is odd or even
+## Your function should take 1 parameter and return "The number x is odd/even number"
+## You might have to google modulus operator
+
+
+#######################
+## RESEARCH MATERIAL ##
+#######################
+
+## I do not expect fot you to understand these concepets right away but diving into them beforehand 
+## will allow you to understand it easier next time you encounter these concepts.
+## If it helps you can write an example along with your explanation in code by adding comments like so:
+
+someVarialbe = 'some complicated concept' ## this is the comment explaining my thought
+
+##----------------------------------------------------##
+
+## 1. What is scope? And how can I be limited by scope?
+
+## 2. What is an array?
+
+## 3. What is an object?
+
+
+########################
+##  STRETCH EXERCISES ##
+########################
+
+## 1. Write a function utlizing an array and returning a modified version of said array.
+
+## 2. Write a function utlizing an object and returning a modified version of said object.
+
